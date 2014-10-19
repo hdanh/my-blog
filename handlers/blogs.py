5a241d2608225d8ec5204214bb973bd3ec9e8d49
hdanh <danh.hoang1991@gmail.com>
2014-10-19 23:36:13 +0700

    commit
    use template

---
+++ handlers/blogs.py
@@ -0,0 +1,5 @@
+from base import BaseHandler
+
+class IndexHandler(BaseHandler):
+    def get(self):
+        self.render_template('views/blogs/index.html')
--- main.py
+++ main.py
@@ -5,13 +5,14 @@ Created on Sep 5, 2014
 '''
 
 import os
-
+import sys
 import webapp2
 import jinja2
 
 #5655edb0-e107-42ba-8109-9b7837e078be - just a secret key
 
 root_dir = os.path.dirname(__file__)
+sys.path.insert(0, 'libs')
 
 config = {
   'webapp2_extras.auth': {
@@ -27,5 +28,6 @@ app = webapp2.WSGIApplication([
     webapp2.Route('/', handler='handlers.home.MainHandler', name='home'),
     webapp2.Route('/contact', handler='handlers.contact.ContactHandler', name='contact'),
     webapp2.Route('/signup', handler='handlers.signup.SignupHandler', name='signup'),
-    webapp2.Route('/login', handler='handlers.login.LoginHandler', name='login')
+    webapp2.Route('/login', handler='handlers.login.LoginHandler', name='login'),
+    webapp2.Route('/blogs', handler='handlers.blogs.IndexHandler', name='blogs')
 ], config=config)
